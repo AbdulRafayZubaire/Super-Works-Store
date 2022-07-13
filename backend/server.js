47ff872c20a65a47479f5d81dc88d2e5e73f0728
AbdulRafay <abdurrafaykhawer10@gmail.com>
2022-07-13 10:54:12 -0700

    still there

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-  5000,
+  PORT,
   console.log(
     `server listening in ${process.env.NODE_ENV} mode on port ${PORT}...`.yellow
     )
